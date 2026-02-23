97323166d42643d8df10d1d22825c9163ef7794f
ufff-333 <konst.an.fe.ro@gmail.com>
2026-02-23 21:41:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log("Hello from script.js!");
 
-const h1 = document.querySelector('h1');
-h1.addEventListener('click', () => {
-  alert('You clicked the heading!');
+const title = document.getElementById("title");
+
+title.style.cursor = "pointer"; // shows it's clickable
+
+title.addEventListener("click", function () {
+  location.reload();
 });
-    document.getElementById("title").addEventListener("click", function() {
-    location.reload();
-  });
